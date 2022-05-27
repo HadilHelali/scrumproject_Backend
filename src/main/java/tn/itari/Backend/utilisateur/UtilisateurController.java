2ca4487d780c96d683cb54959eb6d45d9dcc4103
HadilHelali <helalihadil4@gmail.com>
2022-05-27 16:05:39 +0100

    Adding the backend part for the account screen

--- src/main/java/tn/itari/Backend/utilisateur/UtilisateurController.java
+++ src/main/java/tn/itari/Backend/utilisateur/UtilisateurController.java
@@ -1,8 +1,13 @@
 package tn.itari.Backend.utilisateur;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +25,27 @@ public class UtilisateurController {
     public Utilisateur Login(@RequestBody Utilisateur user) {
         Utilisateur oldUSer = utilisateurRepository.findByEmailAndPassword(user.email, user.password);
         return oldUSer;
+    }
+
+    @GetMapping("/userInfo")
+    public String UserInfo (@RequestParam(name = "email") String email){
+        String Name = utilisateurRepository.findByEmail(email).name;
+        return Name ;
+    }
+
+    @GetMapping("/getId")
+    public Integer getUserId(@RequestParam(name = "email") String email){
+        Integer id = utilisateurRepository.findByEmail(email).id ;
+        return id ;
+    }
+
+    @PostMapping("/update")
+    public Utilisateur update(@RequestBody Utilisateur user){
+        Utilisateur oldUser = utilisateurRepository.findById(user.id).get();
+        System.out.println(oldUser.id);
+        oldUser.name = user.name ;
+        oldUser.email = user.email ;
+        utilisateurRepository.save(oldUser);
+        return oldUser ;
     }
 }
--- src/main/java/tn/itari/Backend/utilisateur/UtilisateurRepository.java
+++ src/main/java/tn/itari/Backend/utilisateur/UtilisateurRepository.java
@@ -1,7 +1,11 @@
 package tn.itari.Backend.utilisateur;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UtilisateurRepository extends JpaRepository<Utilisateur,Integer> {
     Utilisateur findByEmailAndPassword(String email,String password);
+    Utilisateur findByEmail(String email);
+    Optional<Utilisateur> findById(Integer id);
 }
